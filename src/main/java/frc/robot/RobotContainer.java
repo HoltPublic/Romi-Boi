a4b4ddcb58a825eed1dd17059ed7c42a88e4e552
Nixis <gager198@gmail.com>
2021-11-21 14:36:11 -0500

    added more truth

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;//gage is a loser
+package frc.robot;
+// gage is a loser
+// no u
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
